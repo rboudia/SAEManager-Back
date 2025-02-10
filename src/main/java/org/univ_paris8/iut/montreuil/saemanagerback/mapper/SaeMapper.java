aae45fdf26506a1d12ff771d0567bb6d0ec1b44c
fsiddiqui <117906871+Faraz1306@users.noreply.github.com>
2025-02-10 13:26:48 +0100

    Mapper Sae

---
+++ src/main/java/org/univ_paris8/iut/montreuil/saemanagerback/mapper/SaeMapper.java
@@ -0,0 +1,39 @@
+package org.univ_paris8.iut.montreuil.saemanagerback.mapper;
+
+import org.univ_paris8.iut.montreuil.saemanagerback.dto.SaeDTO;
+import org.univ_paris8.iut.montreuil.saemanagerback.entity.Sae;
+
+public class SaeMapper {
+
+    public SaeDTO toDTO(Sae entity) {
+
+        SaeDTO dto = new SaeDTO(
+                entity.getIdSAE(),
+                entity.getNomSae(),
+                entity.getAnneeUniversitaire(),
+                entity.getSemestreUniversitaire(),
+                entity.getSujet(),
+                entity.getDateModificationSujet(),
+                entity.getResponsable()
+        );
+
+        return dto;
+    }
+
+    public Sae toEntity(SaeDTO dto) {
+
+        Sae entity = new Sae();
+
+        entity.setIdSAE(dto.getIdSae());
+        entity.setNomSae(dto.getNomSae());
+        entity.setAnneeUniversitaire(dto.getAnneeUniversitaire());
+        entity.setSemestreUniversitaire(dto.getSemestreUniversitaire());
+        entity.setSujet(dto.getSujet());
+        entity.setDateModificationSujet(dto.getDateModificationSujet());
+        entity.setResponsable(dto.getResponsablesSae());
+
+        return entity;
+
+    }
+
+}
