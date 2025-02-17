26f79782a981ebe12447687063bc408649f59dfa
emrebondy <gucluemre976@gmail.com>
2025-02-17 17:51:44 +0100

    recherche personne par prenom

--- src/main/java/org/univ_paris8/iut/montreuil/saemanagerback/controller/PersonneController.java
+++ src/main/java/org/univ_paris8/iut/montreuil/saemanagerback/controller/PersonneController.java
@@ -39,6 +39,11 @@ public class PersonneController {
         return personneService.getPersonnesByEstProf(0);
     }
 
+    @GetMapping("/etudiant/name")
+    public List<PersonneDTO> getEtudiantByName(@RequestParam String name) {
+        return personneService.getPersonneByName(name);
+    }
+
     @PutMapping("/prof/ajouterProf")
     public ResponseEntity<String> ajouterProf(@RequestBody List<Integer> idsPersonne) {
         try {
@@ -67,4 +72,6 @@ public class PersonneController {
     public ResponseEntity<PersonneDTO> addPersonne(@RequestBody PersonneDTO personneDTO) {
         return ResponseEntity.ok(personneService.addPersonne(personneDTO));
     }
+
+
 }
