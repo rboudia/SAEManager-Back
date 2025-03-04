6f4100b48990c26f44a74dba05649d52183f77c7
rayanboudia <boudia.rayan77@gmail.com>
2025-03-04 21:47:09 +0100

    Ajout d'un paramètre dans le controller des groupes pour l'accès au front

--- src/main/java/org/univ_paris8/iut/montreuil/saemanagerback/controller/GroupeController.java
+++ src/main/java/org/univ_paris8/iut/montreuil/saemanagerback/controller/GroupeController.java
@@ -2,15 +2,13 @@ package org.univ_paris8.iut.montreuil.saemanagerback.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.univ_paris8.iut.montreuil.saemanagerback.dto.GroupeDTO;
 import org.univ_paris8.iut.montreuil.saemanagerback.service.GroupeService;
 
 @RestController
 @RequestMapping("groupe")
+@CrossOrigin(origins = "http://localhost:4200")
 public class GroupeController {
 
     private final GroupeService groupeService;
